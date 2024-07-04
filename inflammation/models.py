865be227a1f676ea31eb7c860bbd9a5619fbce26
kikhiam <majed.kikhia@gmail.com>
2024-07-04 13:11:29 +0200

    adding tests and fixing syntax

--- inflammation/models.py
+++ inflammation/models.py
@@ -21,7 +21,8 @@ def load_csv(filename):
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array for each day.
 
-   :param data: A 2D data array with inflammation data (each row contains measurements for a single patient across all days).
+   :param data: A 2D data array with inflammation data (each row contains measurements 
+   for a single patient across all days).
    :returns: An array of mean values of measurements for each day.
    """
     return np.mean(data, axis=0)
@@ -36,3 +37,18 @@ def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
 
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    max_data = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / max_data[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
